4e4132fcdd5af597b1143b53cc03d006e7804610
husaininazeer <husaini.nazeer@gmail.com>
2021-09-08 11:56:01 +0100

    added test books to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-console.log("my library before:", myLibrary)
 
 function Book(title, author, numOfPages, status){
     this.title = title;
@@ -23,6 +22,49 @@ function addBookToLib(){
     myLibrary.push(newBook);
 }
 
-addBookToLib();
-// test it works:
-console.log("my library after:", myLibrary)
+// addBookToLib();
+
+///////////////////////////////////////////////
+myLibrary.push(new Book('test1', 'testauth1',123, 'no'));
+myLibrary.push(new Book('test2', 'testauth2',456, 'no'));
+myLibrary.push(new Book('test3', 'testauth3',789, 'no'));
+
+// console.log(myLibrary);
+
+///////////////////////////////////////////////
+
+
+// const newBook = document.createTextNode(`${myLibrary[0]['title']}`);
+
+// libDiv.appendChild(newBook);
+
+// select the library div
+const libDiv = document.querySelector('div#library');
+
+
+// for each book in myLibrary array
+for (i = 0; i < myLibrary.length; i++){
+    // make h4 for title, p for author, num of pages and status
+    let newBookDiv = document.createElement('div');
+    newBookDiv.className = "book"
+    
+    let bookTitle = document.createElement('h3');
+    bookTitle.textContent = `Name: ${myLibrary[i]['title']}`;
+    newBookDiv.appendChild(bookTitle);
+    
+    let bookAuthor = document.createElement('p'); 
+    bookAuthor.textContent = `Author: ${myLibrary[i]['author']}`;
+    newBookDiv.appendChild(bookAuthor);
+    
+    let bookNumPages = document.createElement('p'); 
+    bookNumPages.textContent = `Number of Pages: ${myLibrary[i]['numOfPages']}`;
+    newBookDiv.appendChild(bookNumPages);
+    
+    let bookStatus = document.createElement('p'); 
+    bookStatus.textContent = `Status: ${myLibrary[i]['status']}`;
+    newBookDiv.appendChild(bookStatus);
+    
+    
+    libDiv.appendChild(newBookDiv);
+}
+
