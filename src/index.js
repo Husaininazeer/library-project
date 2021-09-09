3df76e694d7f67f39183bb964144693a2a32aa65
husaininazeer <husaini.nazeer@gmail.com>
2021-09-09 17:21:51 +0100

    changed from procedural to OO-based code for displaying content on page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 let myLibrary = [];
 
-function Book(title, author, numOfPages, status){
-    this.title = title;
+function Book(title, author, pages, status){
+    this.title= title;
     this.author = author;
-    this.numOfPages = numOfPages;
+    this.pages = pages;
     this.status = status;
 }
 
@@ -12,11 +12,11 @@ function addBookToLib(){
     
     const title = prompt("Enter the title of the book:");
     const author = prompt("Enter the author's name:");
-    const numOfPages = prompt("How many pages are there?");
+    const pages = prompt("How many pages are there?");
     const status = prompt("Have you completed it?");
 
     // make a new book object with input
-    const newBook = new Book(title, author, numOfPages, status);
+    const newBook = new Book(title, author, pages, status);
 
     // store book in lib
     myLibrary.push(newBook);
@@ -25,6 +25,8 @@ function addBookToLib(){
 // addBookToLib();
 
 ///////////////////////////////////////////////
+// adding test book values
+
 myLibrary.push(new Book('test1', 'testauth1',123, 'no'));
 myLibrary.push(new Book('test2', 'testauth2',456, 'no'));
 myLibrary.push(new Book('test3', 'testauth3',789, 'no'));
@@ -43,28 +45,55 @@ const libDiv = document.querySelector('div#library');
 
 
 // for each book in myLibrary array
-for (i = 0; i < myLibrary.length; i++){
-    // make h4 for title, p for author, num of pages and status
-    let newBookDiv = document.createElement('div');
-    newBookDiv.className = "book"
-    
-    let bookTitle = document.createElement('h3');
-    bookTitle.textContent = `Name: ${myLibrary[i]['title']}`;
-    newBookDiv.appendChild(bookTitle);
-    
-    let bookAuthor = document.createElement('p'); 
-    bookAuthor.textContent = `Author: ${myLibrary[i]['author']}`;
-    newBookDiv.appendChild(bookAuthor);
-    
-    let bookNumPages = document.createElement('p'); 
-    bookNumPages.textContent = `Number of Pages: ${myLibrary[i]['numOfPages']}`;
-    newBookDiv.appendChild(bookNumPages);
-    
-    let bookStatus = document.createElement('p'); 
-    bookStatus.textContent = `Status: ${myLibrary[i]['status']}`;
-    newBookDiv.appendChild(bookStatus);
-    
-    
-    libDiv.appendChild(newBookDiv);
-}
+// for (let i = 0; i < myLibrary.length; i++){
+//     // make h4 for title, p for author, num of pages and status
+//     let newBookDiv = document.createElement('div');
+//     newBookDiv.className = "book"
 
+    // let bookTitle = document.createElement('h3');
+    // bookTitle.textContent = `Name: ${myLibrary[i]['title']}`;
+    // newBookDiv.appendChild(bookTitle);
+
+//     let bookAuthor = document.createElement('p'); 
+//     bookAuthor.textContent = `Author: ${myLibrary[i]['author']}`;
+//     newBookDiv.appendChild(bookAuthor);
+
+//     let bookNumPages = document.createElement('p'); 
+//     bookNumPages.textContent = `Number of Pages: ${myLibrary[i]['pages']}`;
+//     newBookDiv.appendChild(bookNumPages);
+
+//     let bookStatus = document.createElement('p'); 
+//     bookStatus.textContent = `Status: ${myLibrary[i]['status']}`;
+//     newBookDiv.appendChild(bookStatus);
+
+//     libDiv.appendChild(newBookDiv);
+// }
+
+
+for (let i = 0; i < myLibrary.length; i++){
+    // for each book in myLibrary make a newBookDiv
+    let newBookDiv = document.createElement('div');
+    newBookDiv.className = "book";
+
+    for (const [key, value] of Object.entries(myLibrary[i])){
+        console.log(`${key}:${value}`);
+
+        if(key === 'title'){
+            // add a title h4 tag if key it title
+            var bookTitle = document.createElement('h3');
+            bookTitle.textContent = `${key}: ${value}`;
+            
+            newBookDiv.appendChild(bookTitle);
+
+        } else if (key){
+            // add a p tag for everything else
+            var bookPTags = document.createElement('p');
+            bookPTags.textContent = `${key}: ${value}`
+            
+            newBookDiv.appendChild(bookPTags);
+        }
+            
+        libDiv.appendChild(newBookDiv)
+    }
+
+}
