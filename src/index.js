206ccf78aa8e8f7057044eb162a4e83817d586b5
husaininazeer <husaini.nazeer@gmail.com>
2021-09-14 06:47:15 +0100

    displaying text elements on DOM

--- src/index.js
+++ src/index.js
@@ -1,84 +1,59 @@
 let myLibrary = [];
 
-function Book(title, author, pages, status) {
+// making the book constructor
+function Book(title, author, pageNumbers, readStatus) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.status = status;
+  this.pageNumbers = pageNumbers;
+  this.readStatus = readStatus;
 }
 
-///////////////////////////////////////////////
-// ADDING BOOK OBJ TO LIBRARY
-///////////////////////////////////////////////
-
-function addBookToLib() {
-  // take in user input
-  const title = prompt("Enter the title of the book:");
-  const author = prompt("Enter the author's name:");
-  const pages = prompt("How many pages are there?");
-  const status = prompt("Have you completed it?");
-
-  // make a new book object with input
-  const newBook = new Book(title, author, pages, status);
-
-  // store book in lib
-  myLibrary.push(newBook);
-}
-
-///////////////////////////////////////////////
-// ADDING TEST BOOK VALUES
-///////////////////////////////////////////////
-myLibrary.push(
-  new Book("The great big book of idiocy", "Idiot Shellmann", 123, "no")
+// making a test books
+const harryPotter = new Book("Harry Potter", "J.K. Rowling", 200, true);
+const gameofThrones = new Book("A Game of thrones", "G. R. Martin", 600, false);
+const aliceInWonderland = new Book(
+  "Alice's adventures in wonderland",
+  "Lewis Caroll",
+  240,
+  true
 );
-myLibrary.push(new Book("test2", "testauth2", 456, "no"));
-myLibrary.push(new Book("test3", "testauth3", 789, "no"));
 
-///////////////////////////////////////////////
-// DISPLAYING THE BOOKS IN DOM
-///////////////////////////////////////////////
-
-// select the library div
-const libDiv = document.querySelector("div#library");
-
-for (let i = 0; i < myLibrary.length; i++) {
-    // for each book in myLibrary make a newBookDiv
-    let newBookDiv = document.createElement("div");
-    newBookDiv.className = "book";
-    
-    for (const [key, value] of Object.entries(myLibrary[i])) {
-        //   TODO: Refactor this into a switch statement
-        if (key === "title") {
-            // add a title h4 tag if key it title
-            var bookTitle = document.createElement("h3");
-            bookTitle.textContent = `${value}`;
-            
-            newBookDiv.appendChild(bookTitle);
-        } else if (key) {
-            // add a p tag for everything else
-            var bookPTags = document.createElement("p");
-            bookPTags.textContent = `${value}`;
-            
-            newBookDiv.appendChild(bookPTags);
-        }
-        
-        libDiv.appendChild(newBookDiv);
-    }
+// adding book to mylibrary array
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
-///////////////////////////////////////////////
-// OPENING AND CLOSING THE POPUP FORM
-///////////////////////////////////////////////
+// testing they are appended to array correctly
+addBookToLibrary(aliceInWonderland);
+addBookToLibrary(harryPotter);
+addBookToLibrary(gameofThrones);
 
-function openForm() {
-    document.getElementById("popupform").style.display = "block";
-};
+// for every book in the my library array
+myLibrary.forEach((book) => {
+  // create a book div
+  const bookDiv = document.createElement("div");
+  bookDiv.classList.add("bookDiv");
 
-function closeForm() {
-    document.getElementById("loginPopup").style.display = "none";
-    // TODO: Add click anywhere to disappear functionality here
-};
+  // for every attribute, make a node
+  for (const [attrKey, attr] of Object.entries(book)) {
+    switch (attrKey) {
+      case "title":
+        const bookAttrHeader = document.createElement("h3");
+        bookAttrHeader.textContent = `${attr}`;
+        bookDiv.appendChild(bookAttrHeader);
+        break;
 
-///////////////////////////////////////////////
-// 
-///////////////////////////////////////////////
+      default:
+        const bookAttr = document.createElement("p");
+        bookAttr.textContent = `${attr}`;
+        bookDiv.appendChild(bookAttr);
+        break;
+    }
+  }
+
+  // access the library div
+  const libraryDiv = document.querySelector("#libraryDiv");
+  libraryDiv.appendChild(bookDiv);
+});
+
+// append attribute div as a child
