fe4b6259aba2976e8d247e579c4433f14046208c
husaininazeer <husaini.nazeer@gmail.com>
2021-09-16 17:44:20 +0100

    form adds to DOM and then resets

--- src/index.js
+++ src/index.js
@@ -10,32 +10,27 @@ function Book(title, author, pageNumbers, readStatus) {
 
 // making a test books
 const harryPotter = new Book("Harry Potter", "J.K. Rowling", 200, true);
-const gameofThrones = new Book("A Game of thrones", "G. R. Martin", 600, false);
-const theBFG = new Book("The B.F.G", "Roald Dahl", 600, false);
 const aliceInWonderland = new Book(
   "Alice's adventures in wonderland",
   "Lewis Caroll",
   240,
-  true
+  false
 );
 
 // adding book to mylibrary array
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+// testing they are appended to array correctly
+addBookToLibrary(aliceInWonderland);
+addBookToLibrary(harryPotter);
+displayLibInDOM();
 
 // function to open form for adding book
 function openForm() {
   const formDiv = document.querySelector("#formDiv");
   formDiv.style.display = "block";
 }
-
-// testing they are appended to array correctly
-addBookToLibrary(aliceInWonderland);
-addBookToLibrary(harryPotter);
-addBookToLibrary(theBFG);
-addBookToLibrary(gameofThrones);
-displayLibInDOM();
 
 function displayLibInDOM() {
   // for every book in the my library array
@@ -67,8 +62,37 @@ function displayLibInDOM() {
   });
 }
 
-// take input from the HTML form
-function formToBook() {
+function displayLastBookInDOM() {
+  // for every book in the my library array
+  // create a book div
+  const book = myLibrary[myLibrary.length - 1];
+  const bookDiv = document.createElement("div");
+  bookDiv.classList.add("bookDiv");
+
+  // for every attribute, make a node
+  for (const [attrKey, attr] of Object.entries(book)) {
+    switch (attrKey) {
+      case "title":
+        const bookAttrHeader = document.createElement("h3");
+        bookAttrHeader.textContent = `${attr}`;
+        bookDiv.appendChild(bookAttrHeader);
+        break;
+
+      default:
+        const bookAttr = document.createElement("p");
+        bookAttr.textContent = `${attr}`;
+        bookDiv.appendChild(bookAttr);
+        break;
+    }
+  }
+
+  // access the library div, append book div as a child
+  const libraryDiv = document.querySelector("#libraryDiv");
+  libraryDiv.appendChild(bookDiv);
+}
+
+// take input from the HTML form, display in DOM and reset form input
+function formToBook(event) {
   const formTitle = document.querySelector("#formTitle").value;
   const formAuthor = document.querySelector("#formAuthor").value;
   const formPageNumbers = document.querySelector("#formPageNumbers").value;
@@ -79,10 +103,12 @@ function formToBook() {
     formPageNumbers,
     formReadStatus
   );
+  event.preventDefault();
+  document.querySelector("form").reset();
   addBookToLibrary(newBook);
   console.log(myLibrary);
-  displayLibInDOM();
-  // FIXME probably needs addevent listener rather than submit function
-  // element appears then disappears
-  // or try decoupling the above function
+  displayLastBookInDOM();
 }
+
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", formToBook, false);
