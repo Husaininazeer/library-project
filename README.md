9ab2b81a8512c3a30fade3f1474a012fbfba8f35
husaininazeer <husaini.nazeer@gmail.com>
2021-09-18 07:13:07 +0100

    remove button works, although not based on bookID

--- README.md
+++ README.md
@@ -1,25 +1,33 @@
-# Library project 
+# Library project
+
 (still working on the name..)
 
 A simple web app that keeps track of the users reading list.
 
 ### Features:
+
 - Lists books on the shelf as cards
 
 ### Tech Used:
+
 - HMTL & CSS
 - Vanilla JavaScript
 - Git
+- Vim
 
 ### Things I have learnt:
+
 - [x] OOP - object constructors
 - [x] DOM manipulation using JS
+- [x] Refactoring code - decoupling methods
 - [x] Emmet abbreviations for common actions
 
 ### Future plans:
+
 - Add styling
+- Use WebStorageAPI for persistence
 - Test using Testing framework
 - Deployment
 - Extra features:
-    - Reading goal tracking
-    - OAuth integration
+  - Reading goal tracking
+  - OAuth integration
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+// TODO list:
+// get a toggle working for the read status
+//   use prototypical inheritance
+// get webstorageapi working
+// add styling
+
 let myLibrary = [];
 
 // making the book constructor
 function Book(title, author, pageNumbers, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pageNumbers = pageNumbers;
-  this.readStatus = readStatus;
+  return { title, author, pageNumbers, readStatus };
 }
 
 // adding book to mylibrary array
@@ -19,62 +22,68 @@ function openForm() {
   formDiv.style.visibility = "visible";
 }
 
-// function to hold all the content of a book
-const createBookDiv = (book) => {
-  // create a book div
-  const bookDiv = document.createElement("div");
-  bookDiv.classList.add("bookDiv");
-
-  // for book every attribute, make a node
-  for (const [attrKey, attr] of Object.entries(book)) {
-    switch (attrKey) {
-      case "title":
-        const bookAttrHeader = document.createElement("h3");
-        bookAttrHeader.textContent = `${attr}`;
-        bookDiv.appendChild(bookAttrHeader);
-        break;
-
-      default:
-        const bookAttr = document.createElement("p");
-        bookAttr.textContent = `${attr}`;
-        bookDiv.appendChild(bookAttr);
-        break;
-    }
-  }
-  return bookDiv;
-};
-
 // each container contains a bookDiv (with the content) and a removalButton
-const displayBookContainer = (book, removalButton, bookDiv) => {
+const displayBookContainer = (book) => {
   const bookContainer = document.createElement("div");
   bookContainer.classList.add("bookContainer");
 
   const bookID = myLibrary.indexOf(book);
-
-  bookContainer.setAttribute("data-bookID", `${bookID}`);
-  bookContainer.appendChild(removalButton);
-  bookContainer.appendChild(bookDiv);
-
   const libraryDiv = document.querySelector("#libraryDiv");
   libraryDiv.appendChild(bookContainer);
-};
 
-const createRemovalButton = (book) => {
-  const bookID = myLibrary.indexOf(book);
-  // adding a button for removing
-  const removalButton = document.createElement("button");
-  removalButton.classList.add("removalButton");
-  removalButton.setAttribute("id", "removalButton");
-  removalButton.setAttribute("data-bookID", `${bookID}`);
-  removalButton.textContent = "Remove";
-  // bookDiv.appendChild(removalButton);
+  // Functions to make content
 
-  // removing and entry from the DOM
-  removalButton.addEventListener("click", (ev) => {
-    console.log("removed book", bookID);
-  });
+  const createRemovalButton = (book) => {
+    const bookID = myLibrary.indexOf(book);
+    // adding a button for removing
+    const removalButton = document.createElement("button");
+    removalButton.classList.add("removalButton");
+    removalButton.setAttribute("id", "removalButton");
+    removalButton.setAttribute("data-bookID", `${bookID}`);
+    removalButton.textContent = "Remove";
 
-  return removalButton;
+    // removing and entry from the DOM
+    removalButton.addEventListener("click", (ev) => {
+      myLibrary.splice(bookID, 1);
+      // bookContainer.style.visibility = "hidden";
+      while (bookContainer.firstChild) {
+        bookContainer.removeChild(bookContainer.lastChild);
+      }
+      libraryDiv.removeChild(bookContainer);
+      console.log("removed book", bookID, myLibrary);
+    });
+
+    return removalButton;
+  };
+
+  // function to hold all the content of a book
+  const createBookDiv = (book) => {
+    // create a book div
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("bookDiv");
+
+    // for book every attribute, make a node
+    for (const [attrKey, attr] of Object.entries(book)) {
+      switch (attrKey) {
+        case "title":
+          const bookAttrHeader = document.createElement("h3");
+          bookAttrHeader.textContent = `${attr}`;
+          bookDiv.appendChild(bookAttrHeader);
+          break;
+
+        default:
+          const bookAttr = document.createElement("p");
+          bookAttr.textContent = `${attr}`;
+          bookDiv.appendChild(bookAttr);
+          break;
+      }
+    }
+    return bookDiv;
+  };
+
+  bookContainer.setAttribute("data-bookID", `${bookID}`);
+  bookContainer.appendChild(createRemovalButton(book));
+  bookContainer.appendChild(createBookDiv(book));
 };
 
 console.log(myLibrary);
@@ -95,11 +104,7 @@ function formToBook(event) {
   document.querySelector("form").reset();
   addBookToLibrary(newBook);
   console.log(myLibrary);
-  displayBookContainer(
-    newBook,
-    createRemovalButton(newBook),
-    createBookDiv(newBook)
-  );
+  displayBookContainer(newBook);
 }
 
 const submitButton = document.querySelector("#submit");
